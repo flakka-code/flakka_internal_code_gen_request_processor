74d46f796d0406cb5a2db32bf0ddf65c5f02abc8
ryanhanks-bestow <ryan.hanks@bestow.world>
2024-04-28 14:09:04 -0500

    chore: initial commit

---
+++ lib/src/flakka_internal_code_gen_request_processor.dart
@@ -0,0 +1,7 @@
+/// {@template flakka_internal_code_gen_request_processor}
+/// A Very Good Project created by Very Good CLI.
+/// {@endtemplate}
+class FlakkaInternalCodeGenRequestProcessor {
+  /// {@macro flakka_internal_code_gen_request_processor}
+  const FlakkaInternalCodeGenRequestProcessor();
+}
---
+++ test/src/flakka_internal_code_gen_request_processor_test.dart
@@ -0,0 +1,11 @@
+// ignore_for_file: prefer_const_constructors
+import 'package:flakka_internal_code_gen_request_processor/flakka_internal_code_gen_request_processor.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('FlakkaInternalCodeGenRequestProcessor', () {
+    test('can be instantiated', () {
+      expect(FlakkaInternalCodeGenRequestProcessor(), isNotNull);
+    });
+  });
+}
